83bce9448c13561f67486371cb245f7f7a6a3aee
PierreLeidbring <pierre.leidbring@gmail.com>
2020-04-09 16:08:05 +0200

    handle external interrupts

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,30 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use os::{println, init};
+use os::{println, init, hlt_loop};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World!\n");
-    
-    init();
-    
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    println!("Hello World!");
 
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
-    };
+    init();
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
 
     #[cfg(test)]
     test_main();
 
-    println!("It did not crash?!\n");
-    
-
-    loop{}
+    println!("It did not crash?!");
+    hlt_loop();
 }
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop{}
+    hlt_loop();
 }
 
 #[cfg(test)]
