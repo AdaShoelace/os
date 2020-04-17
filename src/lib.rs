7d8fec0291d4d01f0d2ed5bea451d6af8df371db
PierreLeidbring <pierre.leidbring@gmail.com>
2020-04-17 16:14:58 +0200

    heap allocation

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,16 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
+
+extern crate alloc;
 
 pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod gdt;
 pub mod memory;
+pub mod allocator;
 
 #[cfg(test)]
 use bootloader::{BootInfo, entry_point};
@@ -37,6 +41,11 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
         test();
     }
     exit_qemu(QemuExitCode::Success);
+}
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
 }
 
 pub fn test_panic_handler(info: &PanicInfo) -> ! {
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@
 #![test_runner(os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
 use core::panic::PanicInfo;
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 use bootloader::{BootInfo, entry_point};
 use os::{
     println,
@@ -16,11 +19,49 @@ use os::{
 entry_point!(kernel_main);
 
 pub  fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use x86_64::registers::control::Cr3;
-    use x86_64::{structures::paging::{Page, FrameAllocator}, VirtAddr};
+    use os::allocator;
+    use os::memory::{self, BootInfoFrameAllocator};
+    use x86_64::{
+        structures::paging::{
+            mapper::MapToError,
+            FrameAllocator,
+            Mapper,
+            Page,
+            PageTableFlags,
+            Size4KiB
+        },
+        VirtAddr,
+    };
+
     println!("Hello World!");
 
     init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
+
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+
+    println!("vec at: {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 
     #[cfg(test)]
     test_main();
